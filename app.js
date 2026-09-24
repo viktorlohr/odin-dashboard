531d104b732855348ef635c6b59bffecb2ccbb6f
Viktor Lohr <viktor.lohr01@gmail.com>
2026-09-24 16:08:09 +0200

    Style project card

---
+++ app.js
@@ -0,0 +1,17 @@
+const projectOverview = document.querySelector("project-overview");
+for (let i = 0; i < 10; i++) {
+  let projectCard = document.createElement("div");
+  projectCard.classList.add("project-card");
+
+  let projectHeading = document.createElement("div");
+  let projectDescription = document.createElement("div");
+
+  projectHeading.textContent = "This is an awesome project";
+  projectDescription.textContent =
+    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+
+  projectCard.appendChild(projectHeading);
+  projectCard.appendChild(projectDescription);
+
+  // projectOverview.appendChild()
+}
